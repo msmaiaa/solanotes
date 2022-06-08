99a87e8eaea8dcc5532f397cddc9179d98fb1ba2
Matheus Maia <roxx77344@gmail.com>
2022-06-07 23:09:30 -0300

    fetch and add note

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,18 +1,31 @@
+import { Idl, Program, AnchorProvider, web3 } from "@project-serum/anchor";
 import { useEffect, useState } from "react";
+import keypair from "./keypair.json";
+import idl from "./idl.json";
 import "./App.css";
+import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
+
+const { SystemProgram } = web3;
+
+const arr = Object.values(keypair._keypair.secretKey);
+const secret = new Uint8Array(arr);
+const baseAccount = web3.Keypair.fromSecretKey(secret);
+
+const programId = new PublicKey(idl.metadata.address);
+
+const network = clusterApiUrl("devnet");
+
+type Note = {
+  title: string;
+  body: string;
+};
 
 function App() {
   const [noteTitle, setNoteTitle] = useState("");
   const [noteBody, setNoteBody] = useState("");
 
   const [userWallet, setUserWallet] = useState("");
-
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (!noteTitle || !noteBody) return;
-    console.log("title", noteTitle);
-    console.log("body", noteBody);
-  };
+  const [noteList, setNoteList] = useState<Array<Note> | null>([]);
 
   const checkWalletConnection = async () => {
     try {
@@ -30,6 +43,67 @@ function App() {
     } catch (_) {}
   };
 
+  const getProvider = () => {
+    const { solana }: any = window;
+    const connection = new Connection(network, "processed");
+    const provider = new AnchorProvider(connection, solana, {
+      preflightCommitment: "processed",
+    });
+    return provider;
+  };
+
+  const createNoteAccount = async () => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl as Idl, programId, provider);
+      await program.rpc.initialize({
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+        },
+        signers: [baseAccount],
+      });
+      await fetchNotes();
+    } catch (e) {
+      console.log("baseaccount", e);
+    }
+  };
+
+  const createNote = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!noteTitle || !noteBody) return;
+    try {
+      const provider = getProvider();
+      const program = new Program(idl as Idl, programId, provider);
+      await program.rpc.createNote(noteTitle, noteBody, {
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+        },
+      });
+      setNoteTitle("");
+      setNoteBody("");
+      fetchNotes();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const fetchNotes = async () => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl as Idl, programId, provider);
+      const account = await program.account.baseAccount.fetch(
+        baseAccount.publicKey
+      );
+      setNoteList(account.notes as Array<Note>);
+    } catch (e) {
+      console.error("fetchNotes", e);
+      setNoteList(null);
+    }
+  };
+
   useEffect(() => {
     const onLoad = async () => await checkWalletConnection();
     window.addEventListener("load", onLoad);
@@ -37,15 +111,21 @@ function App() {
   }, []);
 
   useEffect(() => {
-    console.log("wallet", userWallet);
     if (userWallet) {
-      console.log("userWallet", userWallet);
+      fetchNotes();
     }
   }, [userWallet]);
 
   const renderWalletConnected = () => {
+    if (!noteList) {
+      return (
+        <button onClick={() => createNoteAccount()}>
+          click here for the one-time account creation
+        </button>
+      );
+    }
     return (
-      <form onSubmit={onSubmit} className="form" autoComplete="off">
+      <form onSubmit={createNote} className="form" autoComplete="off">
         <div className="input__container">
           <label htmlFor="form__title" className="input__label">
             Title
