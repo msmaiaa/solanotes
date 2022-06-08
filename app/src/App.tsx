1933b1e955dd8b4a38cc2a35471cda03251d3408
Matheus Maia <roxx77344@gmail.com>
2022-06-07 21:07:40 -0300

    connect to solana wallet

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [noteTitle, setNoteTitle] = useState("");
   const [noteBody, setNoteBody] = useState("");
+
+  const [userWallet, setUserWallet] = useState("");
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -12,8 +14,37 @@ function App() {
     console.log("body", noteBody);
   };
 
-  return (
-    <div className="page">
+  const checkWalletConnection = async () => {
+    try {
+      const { solana }: any = window;
+      if (solana) {
+        if (solana.isPhantom) {
+          const response = await solana.connect({ onlyIfTruste: true });
+          setUserWallet(response.publicKey.toString());
+        } else {
+          alert("Phantom wallet not found");
+        }
+      } else {
+        alert("Solana wallet not found");
+      }
+    } catch (_) {}
+  };
+
+  useEffect(() => {
+    const onLoad = async () => await checkWalletConnection();
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
+
+  useEffect(() => {
+    console.log("wallet", userWallet);
+    if (userWallet) {
+      console.log("userWallet", userWallet);
+    }
+  }, [userWallet]);
+
+  const renderWalletConnected = () => {
+    return (
       <form onSubmit={onSubmit} className="form" autoComplete="off">
         <div className="input__container">
           <label htmlFor="form__title" className="input__label">
@@ -42,6 +73,15 @@ function App() {
           Send
         </button>
       </form>
+    );
+  };
+
+  const renderWalletDisconnected = () => {
+    return <p style={{ color: "white" }}>Please connect your wallet</p>;
+  };
+  return (
+    <div className="page">
+      {userWallet ? renderWalletConnected() : renderWalletDisconnected()}
     </div>
   );
 }
